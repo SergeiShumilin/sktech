eb8a63945dea4f0c2ffa09bf8a471f0d4e9d1ee0
Sergei <ishumili@gmail.com>
2022-07-18 11:35:56 +0300

    Update test.sh

--- docker/test.sh
+++ docker/test.sh
@@ -4,5 +4,3 @@ sudo docker build -t skoltech .
 sudo docker run -d --name skoltech skoltech tail -f /dev/null
 sudo docker exec skoltech /home/script.sh /home/dracula.txt /home/
 sudo docker exec -it skoltech /bin/bash
-
-
